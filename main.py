5f45348eea315628dac49562a14d14baa918c1f0
Kurisu Cat <64016189+kurisucat@users.noreply.github.com>
2022-04-02 19:08:33 +0800

    Update main.py

--- main.py
+++ main.py
@@ -22,12 +22,12 @@ if os.path.exists("./.env"):
 else:
     envfile = open(".env", "w")
     envfile.write(
-        """ENV_PLACE_USERNAME='["developer_username"]'
-ENV_PLACE_PASSWORD='["developer_password"]'
-ENV_PLACE_APP_CLIENT_ID='["app_client_id"]'
-ENV_PLACE_SECRET_KEY='["app_secret_key"]'
-ENV_DRAW_X_START="x_position_start_integer"
-ENV_DRAW_Y_START="y_position_start_integer"
+        """ENV_PLACE_USERNAME='["开发者账号"]'
+ENV_PLACE_PASSWORD='["开发者密码"]'
+ENV_PLACE_APP_CLIENT_ID='["app客户端id"]'
+ENV_PLACE_SECRET_KEY='["应用_secret_key"]'
+ENV_DRAW_X_START="959"
+ENV_DRAW_Y_START="497"
 ENV_R_START='["0"]'
 ENV_C_START='["0"]\'"""
     )
